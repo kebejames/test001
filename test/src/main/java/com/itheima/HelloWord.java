17b03a3abec87bdcfbccd34ca63b89d99f468ae2
KebeJames <745032926@qq.com>
2021-06-27 21:09:06 +0800

    第一次提交

---
+++ test/src/main/java/com/itheima/HelloWord.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
+        System.out.println("Hello Word");
+    }
+}
